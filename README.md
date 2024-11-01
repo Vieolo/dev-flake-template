486c0e32ba0c3169d4265453bb3190e6238e041b
Ramtin <Ramtin.abadi@gmail.com>
2024-11-01 03:18:29 +0100

    Adjusted the template default

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nix Flake Template for development
+Starting point template for creating a new flake for development
+
+To use run
+```bash
+nix flake init --template github:vieolo/dev-flake-template
+```
--- flake.nix
+++ flake.nix
@@ -1,39 +1,51 @@
 {
-  inputs = {
-    # The latest snapshot of packages
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "Template for development";
 
-    # Pointing to a specific snapshot of the packages to get
-    # specific version of packages
-    # Has been taken from nixhub.io for nodejs 18.18.2
-    nodejs-18-nixpkgs.url = "github:NixOS/nixpkgs/a71323f68d4377d12c04a5410e214495ec598d4c";
-    
-    # To take the snapshot for any package, go to nixhub.io, search for the package,
-    # find the version, and then use the hash
-  };
+  inputs.nixpkgs.url = "nixpkgs";
 
-  outputs = inputs:
-  let
-    # Defining variables before the body of outputs
-    # These packages are for `aarch64-darwin` which is M-series MacOS
+  outputs = { self, nixpkgs }: {
+    templates.default = {
+      # This is the directory that will be copied when the template is used
+      src = self;
+      description = "Starting point for creating a flake for development";
 
-    # Defining pkgs variable for convenience
-    pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-    # Defining nodejs18-pkgs variable for convenience
-    nodejs18-pkgs = inputs.nodejs-18-nixpkgs.legacyPackages.aarch64-darwin;
-  in {
-    # Defining the shell for M-series MacOS
-    devShells.aarch64-darwin.default = pkgs.mkShell {
-      buildInputs = [
-        # nodejs@18
-        nodejs18-pkgs.nodejs_18
-        # bun@latest
-        pkgs.bun
-      ];
+      inputs = {
+        # The latest snapshot of packages
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-      shellHook = ''
-        export PS1='\[\e[2m\][\[\e[1m\]nix-shell\[\e[0;2m\]]\[\e[0m\] \[\e[38;5;220;1m\]\W\[\e[0m\] '
-      '';
+        # Pointing to a specific snapshot of the packages to get
+        # specific version of packages
+        # Has been taken from nixhub.io for nodejs 18.18.2
+        nodejs-18-nixpkgs.url = "github:NixOS/nixpkgs/a71323f68d4377d12c04a5410e214495ec598d4c";
+        
+        # To take the snapshot for any package, go to nixhub.io, search for the package,
+        # find the version, and then use the hash
+      };
+
+      outputs = inputs:
+      let
+        # Defining variables before the body of outputs
+        # These packages are for `aarch64-darwin` which is M-series MacOS
+
+        # Defining pkgs variable for convenience
+        pkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
+        # Defining nodejs18-pkgs variable for convenience
+        nodejs18-pkgs = inputs.nodejs-18-nixpkgs.legacyPackages.aarch64-darwin;
+      in {
+        # Defining the shell for M-series MacOS
+        devShells.aarch64-darwin.default = pkgs.mkShell {
+          buildInputs = [
+            # nodejs@18
+            nodejs18-pkgs.nodejs_18
+            # bun@latest
+            pkgs.bun
+          ];
+
+          shellHook = ''
+            export PS1='\[\e[2m\][\[\e[1m\]nix-shell\[\e[0;2m\]]\[\e[0m\] \[\e[38;5;220;1m\]\W\[\e[0m\] '
+          '';
+        };
+      };
     };
   };
 }
